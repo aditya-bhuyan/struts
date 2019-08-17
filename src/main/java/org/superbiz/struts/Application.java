e80dc1c50b58d9a929ecf5b239074421d70d87b2
aditya-bhuyan <aditya.sunjava@gmail.com>
2019-08-17 11:08:49 +0900

    Added Actuator and Security

--- src/main/java/org/superbiz/struts/Application.java
+++ src/main/java/org/superbiz/struts/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import javax.servlet.Filter;
 
@@ -15,6 +16,7 @@ import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class Application {
 
     public static void main(String... args) {
---
+++ src/main/java/org/superbiz/struts/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package org.superbiz.struts;
+
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
+
+@Configuration
+@Order(1)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .requestMatchers(EndpointRequest.to("info", "health")).permitAll()
+                .requestMatchers(EndpointRequest.toAnyEndpoint()).authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionAuthenticationStrategy(new NullAuthenticatedSessionStrategy())
+        ;
+
+
+    }
+}
